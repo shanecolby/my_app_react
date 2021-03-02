8afc6f7e181802a4cb78a69bff806372bec39b6d
shanecolby <shanecolbyzero8@gmail.com>
2021-03-02 16:45:50 -0600

    add checkbox to maincontent

--- src/components/MainContent.js
+++ src/components/MainContent.js
@@ -6,8 +6,14 @@ function MainContent() {
       <h1>This is the Working App page</h1>
       <ul>
         <li>This is the first step</li>
+        <h6>Click when complete</h6>
+        <input type='checkbox' />
         <li>The is the second step</li>
+        <h6>Click when complete</h6>
+        <input type='checkbox' />
         <li>This is the third step</li>
+        <h6>Click when complete</h6>
+        <input type='checkbox' />
       </ul>
       <h3>Thanks for checking it out!</h3>
     </div>
