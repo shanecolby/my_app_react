77f239d6ca439adbcce1b93dec3fedc841f6bffb
shanecolby <shanecolbyzero8@gmail.com>
2021-03-02 19:38:11 -0600

    add dynamic inline styling to header

--- src/components/Header.js
+++ src/components/Header.js
@@ -5,17 +5,23 @@ function Header() {
   const date = new Date()
   const hours = date.getHours()
   let timeOfDay
+  const styles = {
+    fontSize: 30
+  }
 
   if (hours < 12) {
     timeOfDay = "morning"
+    styles.color = "#04756F"
   } else if (hours >= 12 && hours < 17) {
     timeOfDay = "afternoon"
+    styles.color = "#8914A3"
   } else {
     timeOfDay = "night"
+    styles.color = "#D90000"
   }
 
   return (
-    <header className='navbar'> Good {`${timeOfDay}`}! Welcome to my page...</header>
+    <header className='navbar' style={styles}> Good {`${timeOfDay}`}! Welcome to my page...</header>
   )
 }
 
