42b3fa340189f2150bcdeff7ede4106f0d6d22d9
shanecolby <shanecolbyzero8@gmail.com>
2021-03-04 15:48:14 -0600

    add props to product from randomProducts page

--- src/WorkingApp.js
+++ src/WorkingApp.js
@@ -3,12 +3,17 @@ import React from 'react'
 import MainContent from './components/MainContent'
 import Header from './components/Header'
 import Footer from './components/Footer'
+import Product from "./components/Product"
+import productData from "./components/randomProducts"
+
 
 function WorkingApp() {
+  const productComponents = productData.map(item => <Product key={item.id} product={item} />)
   return (
     <div>
       <Header />
       <MainContent />
+      {productComponents}
       <Footer />
     </div>
   )
