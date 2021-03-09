a1371971467b5aa457bb3c6c5e9418d4f43caacb
shanecolby <shanecolbyzero8@gmail.com>
2021-03-08 19:17:02 -0600

    add footer image

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -2,7 +2,9 @@ import React from 'react'
 
 function Footer() {
   return (
-    <footer className='pageBottom'>This is where the footer goes...</footer>
+    <footer className='pageBottom'>This is where the footer goes...
+      <img src="https://media.istockphoto.com/vectors/summer-vector-illustration-for-site-header-footer-web-banner-flyer-or-vector-id961542724" />
+    </footer>
   )
 }
 
