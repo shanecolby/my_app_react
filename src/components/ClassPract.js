5c47e118218a84e7688864ddf7c2fb9fc58089f1
shanecolby <shanecolbyzero8@gmail.com>
2021-03-09 19:41:33 -0600

    add country

--- src/components/ClassPract.js
+++ src/components/ClassPract.js
@@ -5,7 +5,8 @@ class ClassPract extends React.Component {
     super()
     this.state = {
       name: "Shane",
-      age: "30"
+      age: "30",
+      country: "USA"
     }
   }
   render() {
@@ -13,6 +14,7 @@ class ClassPract extends React.Component {
       <div style={{ textAlign: "center" }}>
         <h3 style={{ color: "#328475" }}>{this.state.name}</h3>
         <h3>{this.state.age}</h3>
+        <h3>{this.state.country}</h3>
       </div >
     )
   }
