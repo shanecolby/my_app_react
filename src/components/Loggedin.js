ceb08c1575ea71fa4ac2599db20b1fdba5311a51
shanecolby <shanecolbyzero8@gmail.com>
2021-03-23 20:56:35 -0500

    add login button and log out with change to text and button text

--- src/components/Loggedin.js
+++ src/components/Loggedin.js
@@ -6,7 +6,17 @@ class Loggedin extends React.Component {
     this.state = {
       isLoggedIn: true
     }
+    this.handleClick = this.handleClick.bind(this)
   }
+
+  handleClick() {
+    this.setState(prevState => {
+      return {
+        isLoggedIn: !prevState.isLoggedIn
+      }
+    })
+  }
+
   render() {
     let wordDisplay
     if (this.state.isLoggedIn) {
@@ -14,9 +24,12 @@ class Loggedin extends React.Component {
     } else {
       wordDisplay = "out"
     }
+    let buttonText = this.state.isLoggedIn ? "Log Out" : "Log In"
     return (
-      <div>
+      <div style={{ margin: "auto", width: 300, padding: 10 }}>
         <h1 onClick={() => console.log("clicked")} style={{ textAlign: "center" }}>You are currently logged {wordDisplay} </h1>
+        <p></p>
+        <button className="button" onClick={this.handleClick}>{buttonText}</button>
       </div>
     )
   }
