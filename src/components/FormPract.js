98ce524709ba1a38768c0f30f6334492db44f147
shanecolby <shanecolbyzero8@gmail.com>
2021-03-30 17:18:47 -0500

    add value to show current state

--- src/components/FormPract.js
+++ src/components/FormPract.js
@@ -19,9 +19,24 @@ class FormPract extends React.Component {
   render() {
     return (
       <form style={{ textAlign: "center" }}>
-        <input type="text" name="firstName" placeholder="First Name" onChange={this.handleChange} />
+        <input
+          type="text"
+          value={this.state.firstName}
+          name="firstName"
+          placeholder="First Name"
+          onChange={this.handleChange}
+        />
+
         <br />
-        <input type="text" name="lastName" placeholder="Last Name" onChange={this.handleChange} />
+
+        <input
+          type="text"
+          value={this.state.lastName}
+          name="lastName"
+          placeholder="Last Name"
+          onChange={this.handleChange}
+        />
+
         <h1>{this.state.firstName} {this.state.lastName}</h1>
       </form>
     )
