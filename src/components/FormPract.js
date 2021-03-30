85792c8a85d4c54b3114b0652959ff3f9b24b964
shanecolby <shanecolbyzero8@gmail.com>
2021-03-30 17:13:35 -0500

    add text box for first and last name while displaying each name together but apart

--- src/components/FormPract.js
+++ src/components/FormPract.js
@@ -3,13 +3,26 @@ import React from "react"
 class FormPract extends React.Component {
   constructor() {
     super()
-    this.state = {}
+    this.state = {
+      firstName: "",
+      lastName: ""
+    }
+    this.handleChange = this.handleChange.bind(this)
+  }
+
+  handleChange(event) {
+    this.setState({
+      [event.target.name]: event.target.value
+    })
   }
 
   render() {
     return (
       <form style={{ textAlign: "center" }}>
-        <input type="text" placeholder="" />
+        <input type="text" name="firstName" placeholder="First Name" onChange={this.handleChange} />
+        <br />
+        <input type="text" name="lastName" placeholder="Last Name" onChange={this.handleChange} />
+        <h1>{this.state.firstName} {this.state.lastName}</h1>
       </form>
     )
   }
