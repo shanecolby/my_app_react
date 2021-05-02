6ad63caa72e3b567c46710aaf526bfd687566e39
shanecolby <shanecolbyzero8@gmail.com>
2021-05-02 07:35:50 -0500

    log in/out functional component

--- src/components/Loggedin.js
+++ src/components/Loggedin.js
@@ -1,45 +1,59 @@
-import React from "react"
+import React, { useState } from "react"
 
-class Loggedin extends React.Component {
-  constructor() {
-    super()
-    this.state = {
-      isLoggedIn: false
-    }
-    this.handleClick = this.handleClick.bind(this)
-  }
+function Loggedin() {
+  const [loggedIn, setLoggedIn] = useState("false")
 
-  handleClick() {
-    this.setState(prevState => {
-      return {
-        isLoggedIn: !prevState.isLoggedIn
-      }
-    })
-  }
 
-  render() {
-    let wordDisplay
-    if (this.state.isLoggedIn) {
-      wordDisplay = "in"
-    } else {
-      wordDisplay = "out"
-    }
-    let buttonText = this.state.isLoggedIn ? "Log Out" : "Log In"
-    return (
-      <div style={{ margin: "auto", width: 300, padding: 10 }}>
-        <h1 onClick={() => console.log("clicked")} style={{ textAlign: "center" }}>You are currently logged {wordDisplay} </h1>
-        <p></p>
-        <button
-          style={{
-            color: "goldenrod",
-            backgroundColor: "navy",
-            outlineColor: "white"
-          }}
-          className="button"
-          onClick={this.handleClick}>{buttonText}</button>
-      </div>
-    )
-  }
+
+  return (
+    <div>
+      <h1></h1>
+      <p></p>
+      <button>Log in</button>
+    </div>
+  )
 }
+
+// class Loggedin extends React.Component {
+//   constructor() {
+//     super()
+//     this.state = {
+//       isLoggedIn: false
+//     }
+//     this.handleClick = this.handleClick.bind(this)
+//   }
+
+//   handleClick() {
+//     this.setState(prevState => {
+//       return {
+//         isLoggedIn: !prevState.isLoggedIn
+//       }
+//     })
+//   }
+
+//   render() {
+//     let wordDisplay
+//     if (this.state.isLoggedIn) {
+//       wordDisplay = "in"
+//     } else {
+//       wordDisplay = "out"
+//     }
+//     let buttonText = this.state.isLoggedIn ? "Log Out" : "Log In"
+//     return (
+//       <div style={{ margin: "auto", width: 300, padding: 10 }}>
+//         <h1 onClick={() => console.log("clicked")} style={{ textAlign: "center" }}>You are currently logged {wordDisplay} </h1>
+//         <p></p>
+//         <button
+//           style={{
+//             color: "goldenrod",
+//             backgroundColor: "navy",
+//             outlineColor: "white"
+//           }}
+//           className="button"
+//           onClick={this.handleClick}>{buttonText}</button>
+//       </div>
+//     )
+//   }
+// }
 
 export default Loggedin
