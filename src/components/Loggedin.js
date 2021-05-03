1bbc8a02d56d9eff74ef8169a7512b747829c9c4
shanecolby <shanecolbyzero8@gmail.com>
2021-05-03 16:42:34 -0500

    refactor

--- src/components/Loggedin.js
+++ src/components/Loggedin.js
@@ -3,13 +3,7 @@ import React from "react"
 // function Loggedin() {
 //   const [loggedIn, setLoggedIn] = useState("false")
 
-// handleClick() {
-//   (prevState => {
-//     return {
-//       isLoggedIn: !prevState.isLoggedIn
-//     }
-//   })
-// }
+
 
 
 
