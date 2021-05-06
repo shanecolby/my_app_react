1f0bcd3580a39080133afa36fae20485517a3292
shanecolby <shanecolbyzero8@gmail.com>
2021-05-05 20:04:46 -0500

    add button and count with state

--- src/components/StatePract.js
+++ src/components/StatePract.js
@@ -3,11 +3,31 @@ import React, { useState } from "react"
 function StatePract() {
   const [name] = useState("Shane")
   const [age] = useState("30")
+  const [count, setCount] = useState(0)
+
+  function addOne() {
+    setCount(prevCount => prevCount + 1)
+  }
+
+  function subOne() {
+    setCount(prevCount => prevCount - 1)
+  }
+
+  function clear() {
+    setCount(prevCount => prevCount - prevCount)
+  }
+
+
+
 
   return (
     <div>
       <h1>Your name is {name}</h1>
       <h1>You are {age} years old</h1>
+      <h1>{count}</h1>
+      <button onClick={addOne}>Add One</button>
+      <button onClick={subOne}>Subtract One</button>
+      <button onClick={clear}>CLEAR</button>
     </div>
   )
 }
